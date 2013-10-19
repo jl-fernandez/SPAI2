59bef743be06288dbf6511b4ee600e251b10c439
jl-fernandez <fernandezfernandezjl@gmail.com>
2013-10-19 10:24:55 +0200

    commit2

--- src/egit/primera.java
+++ src/egit/primera.java
@@ -5,6 +5,7 @@ public class primera {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ya tengo repositorio");
+		System.out.println("Otro commit");
 	}
 
 }
